77865472742ddacf77a0fe5d9e456e5ad0ef94ae
golubevpasha <golubevpasha@mail.ru>
2019-07-03 19:36:25 +0300

    test_01

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?,String?>{
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName =parts?.getOrNull(1)
+
+        //return Pair(firstName, lastName)
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider : String = " "): String {
+        return ""
+        TODO("not implemented")
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return ""
+        TODO("not implemented")
+    }
+}
